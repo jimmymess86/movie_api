2f152cb4e5eb94749cb66304032cae4016627ded
jimmymess86 <jimmymess86@gmail.com>
2022-02-21 14:22:22 -0500

    msg

--- models.js
+++ models.js
@@ -29,7 +29,7 @@ userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
 
-userSchema.methods.validatePassword = function(pasword) {
+userSchema.methods.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.Password);
 };
 
